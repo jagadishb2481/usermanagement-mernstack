8779f412bc485cc588b552a0a3a57310b2a2e4f0
Jagadish Burri <jagadishb2481@gmail.com>
2023-03-12 19:30:55 -0400

    committing services of express js with mongodb

---
+++ server/model/model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    name: { required: true, type: String },
+    email: { required: true, type: String },
+    age: { required: true, type: Number },
+})
+
+module.exports = mongoose.model('Users', dataSchema)
